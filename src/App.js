44984c513814e53cc0f9d56ffac405f751e31ba3
Vazgen Davtyan <vazgenddavtyan@gmail.com>
2019-02-21 14:15:49 +0400

    add dependencies= bootstrap, reactstrap, react-router-dom, firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Button } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hello </h1>
+        <h1>Davit Ashot Hrant Smbat Vazgen</h1>
+        <Button color="warning">Let's start</Button>
       </div>
     );
   }
