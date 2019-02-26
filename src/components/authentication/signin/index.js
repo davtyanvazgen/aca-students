b5ea8efa8824ebc420ed7e5ac99b99f7dae8f688
Smbat <sampogosyan1995@gmail.com>
2019-02-26 14:19:17 +0400

    new

--- src/components/authentication/signin/index.js
+++ src/components/authentication/signin/index.js
@@ -3,33 +3,33 @@ import "../styles/style.css";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 export default function SignIn() {
-  return (
-    <div id="container">
-      <div className="miniContainer">
-        <Form>
-          <FormGroup>
-            <Label for="exampleEmail">Email address</Label>
-            <Input
-              type="email"
-              name="email"
-              // id="exampleEmail"
-              placeholder="Email"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Password</Label>
-            <Input
-              type="password"
-              name="password"
-              // id="examplePassword"
-              placeholder="Password"
-            />
-          </FormGroup>
-          <Button color="success" block>
-            Sign in
-          </Button>
-        </Form>
-      </div>
-    </div>
-  );
+    return (
+        <div id="container">
+            <div className="miniContainer">
+                <Form>
+                    <FormGroup>
+                        <Label for="exampleEmail">Email address</Label>
+                        <Input
+                            type="email"
+                            name="email"
+                            // id="exampleEmail"
+                            placeholder="Email"
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="examplePassword">Password</Label>
+                        <Input
+                            type="password"
+                            name="password"
+                            // id="examplePassword"
+                            placeholder="Password"
+                        />
+                    </FormGroup>
+                    <Button color="success" block>
+                        Sign in
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,31 +1,34 @@
 import React from "react";
 import { Link, NavLink as RRNavLink } from "react-router-dom";
 import { Button, ButtonGroup, Navbar, Nav, NavLink } from "reactstrap";
+import Buttons from "./buttonsGroup";
 
 export default function Header() {
-  return (
-    <div>
-      <Navbar color="dark" light expand="md">
-        <NavLink to="/" className="text-light" tag={RRNavLink}>
-          ACA students list
-        </NavLink>
+    return (
+        <div>
+            <Navbar color="dark" light expand="md">
+                <NavLink to="/" className="text-light" tag={RRNavLink}>
+                    ACA students list
+                </NavLink>
 
-        <Nav className="ml-auto" navbar>
-          <ButtonGroup>
-            <Link to="/signin">
-              <Button size="sm" outline color="light" className="mr-1">
-                Sign in
-              </Button>
-            </Link>
+                <Buttons />
 
-            <Link to="/signup">
-              <Button size="sm" outline color="light">
-                Sign up
-              </Button>
-            </Link>
-          </ButtonGroup>
-        </Nav>
-      </Navbar>
-    </div>
-  );
+                <Nav className="ml-auto" navbar>
+                    <ButtonGroup>
+                        <Link to="/signin">
+                            <Button size="sm" outline color="light" className="mr-1">
+                                Sign in
+                            </Button>
+                        </Link>
+
+                        <Link to="/signup">
+                            <Button size="sm" outline color="light">
+                                Sign up
+                            </Button>
+                        </Link>
+                    </ButtonGroup>
+                </Nav>
+            </Navbar>
+        </div>
+    );
 }
