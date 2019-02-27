2d540552f4b187b040427bec2b6e265ae0da1e6a
Smbat <sampogosyan1995@gmail.com>
2019-02-27 15:00:32 +0400

    sdfds

--- src/components/authentication/registrationForm/addRegForm.js
+++ src/components/authentication/registrationForm/addRegForm.js
@@ -13,91 +13,88 @@ export default function RegistrationForm(props) {
  const selectedCource = props.selectValue;
  const allStatuses = props.allStatuses;
 
-  function handeleCreateStudent() {
-    console.log(allStatuses);
-   const defaultStatus = allStatuses.find(el =>el.name === "apply");
-    console.log(defaultStatus);
-   let student = {
-        fullName:fullName.value,
+ function handeleCreateStudent() {
+     console.log(allStatuses);
+     const defaultStatus = allStatuses.find(el =>el.name === "apply");
+     console.log(defaultStatus);
+     let student = {
+         fullName:fullName.value,
          phone:phone.value,
          email:email.value,
-           statusName: defaultStatus.name,
-           courceName: selectedCource.name,
+         statusName: defaultStatus.name,
+         courceName: selectedCource.name,
          status:defaultStatus.id,
          cource:selectedCource.id,
          id:v1()
-   }
+     }
 
-   console.log("student = ",student);
-  
-   FireManager.addStudent(student).then(()=>{
-    alert("done!");
-    
-   }
 
-   ).catch(err=>{
-     console.log(err.message)
-   })
+     console.log("student = ",student);
+
+
+     FireManager.addStudent(student).then(()=>{
+         alert("done!");
+     }).catch( err => {
+         console.log(err.message)
+     })
  }
-  return (
-    <>
-      <div id="container">
-        <div className="miniContainer">
-          <Form>
-            <FormGroup>
-              <Label>FullName</Label>
-              <Input
-                {...fullName}
-                type="text"
-                name="firstName"
-              />
-            </FormGroup>
-            
-            <FormGroup>
-              <Label for="exampleEmail">Email address</Label>
-              <Input
-                {...email}
-                type="email"
-                name="email"
-              />
-            </FormGroup>
 
-            <FormGroup>
-              <Label for="examplePassword">Phone</Label>
-              <Input
-                {...phone}
-                placeholder = "+374-00-00-00-00"
-                type="text"
-                name="phone"
-              />
-            </FormGroup>
+ return (
+     <>
+         <div id="container">
+             <div className="miniContainer">
+                 <Form>
+                     <FormGroup>
+                         <Label>FullName</Label>
+                         <Input
+                             {...fullName}
+                             type="text"
+                             name="firstName"
+                         />
+                     </FormGroup>
+                     <FormGroup>
+                         <Label for="exampleEmail">Email address</Label>
+                         <Input
+                             {...email}
+                             type="email"
+                             name="email"
+                         />
+                     </FormGroup>
+                     <FormGroup>
+                         <Label for="examplePassword">Phone</Label>
+                         <Input
+                             {...phone}
+                             placeholder = "+374-00-00-00-00"
+                             type="text"
+                             name="phone"
+                         />
+                     </FormGroup>
+                     <FormGroup>
+                         <Label>Select lesson</Label><br/>
+                         <SelectForm
+                             changeSelectValue = {props.changeSelectValue}
+                             selectValue = {props.selectValue}
+                             changeAllStatuses = {props.changeAllStatuses}
+                             allStatuses = {props.allStatuses}/>
+                     </FormGroup>
+                     <Button color="success" block onClick = {handeleCreateStudent}>
+                         Registration
+                     </Button>
+                 </Form>
+             </div>
+         </div>
+     </>
+ );
 
-            <FormGroup>
-              <Label>Select lesson</Label><br/>
-              <SelectForm 
-                changeSelectValue = {props.changeSelectValue} 
-                selectValue = {props.selectValue} 
-                changeAllStatuses = {props.changeAllStatuses} 
-                allStatuses = {props.allStatuses}/>
-            </FormGroup>
 
-            <Button color="success" block onClick = {handeleCreateStudent}>
-              Registration
-            </Button>
-          </Form>
-        </div>
-      </div>
-    </>
-  );
-
-  function useFormInput(initialValue){
-    const [value,setValue] =useState(initialValue);
-    function handlechange(e){
-      setValue (e.target.value)
-    }
-    return{
-      value,
-      onChange:handlechange
-    }
-  }
+ function useFormInput(initialValue){
+     const [value,setValue] =useState(initialValue);
+     function handlechange(e){
+         setValue (e.target.value)
+     }
+     return{
+         value,
+         onChange:handlechange
+     }
+ }
 }
