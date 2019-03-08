bdddaab07323c5b9c997ae14ba5808f85ec52555
Smbat <sampogosyan1995@gmail.com>
2019-03-08 17:11:46 +0400

    dfdf

--- src/containers/Students.js
+++ src/containers/Students.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import CourcesButton from "../components/students/courcesButtonGroup";
-import StatusesButton from "../components/students/statusesButtonGroup";
+import CourcesButton from "../components/students/courcesButtonGroup"
+import StatusesButton from "../components/students/statusesButtonGroup"
 import StudentCard from "../components/students/studentCard";
 import { setFilter } from "../store/actions";
 import { visibilityFilters } from "../store/actions";
@@ -8,82 +8,87 @@ import Input from "reactstrap/es/Input";
 
 class Students extends Component {
   state = {
-    selectedCource: [],
-    selectedStatus: [],
-    serchedStudents: this.props.students,
-    isSearch: false
+    selectedCource:"",
+    selectedCources:[],
+    selectedStatus:[],
+    searchedStudents: this.props.students,
+    searchValue: "",
   };
 
-  handleSearch = e => {
-    if (e.target.value === "") {
+  // componentDidMount() {
+  //   this.handleSearch();
+  // }
+
+  handleSearch = (value = this.state.searchValue) => {
+    if(value === ""){
       this.setState({
-        isSearch: false
-      });
-      return;
+        searchedStudents: "",
+      })
+      return
     }
-    let { students } = this.props;
-    let value = e.target.value;
+    this.setState({
+      searchValue: value,
+    });
+
+    let {students} = this.props;
+    let tempValue = value;
     let resultArr = [];
-    for (let i = 0; i < students.length; i++) {
+    for(let i = 0; i < students.length; i++){
       let counter = 0;
-      for (let j = 0; j < value.length; j++) {
-        if (students[i].fullName[j] === value[j]) {
-          counter++;
+      for(let j = 0; j < tempValue.length; j++){
+        if(students[i].fullName[j] === tempValue[j]){
+          counter ++;
         }
       }
-      if (counter === value.length) {
+      if(counter === tempValue.length){
         resultArr.push(students[i]);
       }
     }
     this.setState({
-      serchedStudents: resultArr,
-      isSearch: true
-    });
+      searchedStudents: resultArr,
+    })
   };
 
   filterStudents = () => {
-    let { selectedCource, selectedStatus } = this.state;
-    let filters = selectedStatus.length
-      ? [...selectedCource, selectedStatus[0]]
-      : [...selectedCource];
+    debugger;
+    let { selectedCources ,selectedStatus } = this.state;
+    let filters = selectedStatus.length ? [...selectedCources,selectedStatus[0]] : [...selectedCources];
 
-    if (!selectedStatus.length && selectedCource.length) {
-      this.props.dispatch(
-        setFilter(visibilityFilters.SHOW_WITH_COURCES, filters)
-      );
+    if (!selectedStatus.length && selectedCources.length) {
+      this.props.dispatch(setFilter(visibilityFilters.SHOW_WITH_COURCES, filters));
     }
-    if (selectedStatus.length && !selectedCource.length) {
-      this.props.dispatch(
-        setFilter(visibilityFilters.SHOW_WITH_STATUS, filters)
-      );
+    if (selectedStatus.length && !selectedCources.length) {
+      this.props.dispatch(setFilter(visibilityFilters.SHOW_WITH_STATUS, filters));
     }
-    if (selectedStatus.length && selectedCource.length) {
-      this.props.dispatch(
-        setFilter(visibilityFilters.SHOW_WITH_COURCES_AND_STATUS, filters)
-      );
+    if (selectedStatus.length && selectedCources.length) {
+      this.props.dispatch(setFilter(visibilityFilters.SHOW_WITH_COURCES_AND_STATUS, filters));
     }
-    if (!selectedStatus.length && !selectedCource.length) {
+    if (!selectedStatus.length && !selectedCources.length) {
       this.props.dispatch(setFilter(visibilityFilters.SHOW_ALL, filters));
     }
+    this.handleSearch();
   };
 
-  courceStudents = cource => {
-    const { selectedCource } = this.state;
-    if (selectedCource.indexOf(cource.id) === -1) {
-      selectedCource.push(cource.id);
+  courceStudents = (cource = this.state.selectedCource) => {
+    this.setState({
+      selectedCource: cource,
+    })
+    const {selectedCources} = this.state;
+    if (selectedCources.indexOf(cource.id) === -1 && cource) {
+      selectedCources.push(cource.id);
       this.setState({
-        selectedCource
-      });
+        selectedCources
+      })
     } else {
-      selectedCource.splice(selectedCource.indexOf(cource.id), 1);
+      selectedCources.splice(selectedCources.indexOf(cource.id),1);
       this.setState({
-        selectedCource
-      });
+        selectedCources
+      })
     }
     this.filterStudents();
   };
 
-  statuseStudents = status => {
+  statuseStudents = (status) => {
     let { selectedStatus } = this.state;
     if (status !== "all") {
       if (selectedStatus.length) {
@@ -92,7 +97,7 @@ class Students extends Component {
       selectedStatus.push(status.id);
       this.setState({
         selectedStatus
-      });
+      })
     } else {
       selectedStatus.pop();
       this.setState({
@@ -103,48 +108,46 @@ class Students extends Component {
   };
 
   render() {
-    // const {
-    //   withCourcesStudents,
-    //   withStatusStudents,
-    //   showStudentsArr
-    // } = this.state;
+    const { withCourcesStudents,withStatusStudents,showStudentsArr } = this.state;
     return (
-      <div className="container border border-primary">
-        <div className="row">
-          <div className="col-12 border border-primary">
-            <CourcesButton courceStudents={this.courceStudents} />
-            <Input onChange={this.handleSearch} />
+        <div className="container border border-primary">
+          <div className="row">
+            <div className="col-12 border border-primary">
+              <CourcesButton
+                  courceStudents = { this.courceStudents }
+              />
+              <Input onChange={(e)=>this.handleSearch(e.target.value)}></Input>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-2 border border-primary">
+              <StatusesButton
+                  statuseStudents = {this.statuseStudents}
+              />
+            </div>
+            <div className="col-10 row container border border-primary">
+              {this.state.searchedStudents
+                  ? this.state.searchedStudents.map(student => (
+                  <StudentCard
+                      key={ student.id }
+                      student= { student }
+                      repeatFiltering = {this.repeatFiltering}
+                      filterStudents = {this.filterStudents}
+                  />))
+                  : this.props.students && this.props.students.map(student => (
+                  <StudentCard
+                      key={ student.id }
+                      student= { student }
+                      repeatFiltering = {this.repeatFiltering}
+                      filterStudents = {this.filterStudents}
+                  />))
+              }
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col-2 border border-primary">
-            <StatusesButton statuseStudents={this.statuseStudents} />
-          </div>
-          <div className="col-10 row container border border-primary">
-            {!this.state.isSearch
-              ? this.props.students &&
-                this.props.students.map(student => (
-                  <StudentCard
-                    key={student.id}
-                    student={student}
-                    repeatFiltering={this.repeatFiltering}
-                    filterStudents={this.filterStudents}
-                  />
-                ))
-              : this.state.serchedStudents &&
-                this.state.serchedStudents.map(student => (
-                  <StudentCard
-                    key={student.id}
-                    student={student}
-                    repeatFiltering={this.repeatFiltering}
-                    filterStudents={this.filterStudents}
-                  />
-                ))}
-          </div>
-        </div>
-      </div>
-    );
+    )
   }
 }
+
 
 export default Students;
